0b6c0d2eb626424e80dcfb46fc59ce8befe73ea4
rehanardianshahab <rehanardian1@gmail.com>
2026-06-10 23:50:10 +0700

    feat: implement login page and authenticated dashboard route skeleton

--- src/routes/(authentication)/login/+page.svelte
+++ src/routes/(authentication)/login/+page.svelte
@@ -1,13 +1,30 @@
 <script lang="ts">
 	import { zod4Client } from 'sveltekit-superforms/adapters';
 	import { Button, Card } from 'flowbite-svelte';
+	import type { FormOptions } from 'sveltekit-superforms';
 
-	import { schema } from './_types/schema.js';
+	import { schema, type Model } from './_types/schema.js';
 	import FormComponents from './_components/form.svelte';
 	import FormContainer from '../../../components/ui/form-container.svelte';
 	import PageTitle from '../../../components/ui/page-title.svelte';
+	import { goto } from '$app/navigation';
 
 	let { data } = $props();
+
+	const handleSubmit: FormOptions<Model>['onSubmit'] = ({ action, formData }) => {
+		console.log('Form akan dikirim:', formData);
+		action.searchParams.set('client_type', 'desktop');
+		// cancel()
+	};
+
+	const handleUpdated: FormOptions<Model>['onUpdated'] = ({ form }) => {
+		if (form.valid) {
+			console.log('Submit sukses! Data valid.');
+			goto('/dashboard');
+		} else {
+			console.log('Submit gagal karena data tidak valid.');
+		}
+	};
 </script>
 
 <svelte:head>
@@ -20,7 +37,7 @@
 		<FormContainer
 			data={data.form}
 			validators={zod4Client(schema)}
-			options={{ validationMethod: 'oninput' }}
+			options={{ validationMethod: 'oninput', onUpdated: handleUpdated, onSubmit: handleSubmit }}
 		>
 			{#snippet children(sf)}
 				<Card class="flex w-md gap-4 p-6">
